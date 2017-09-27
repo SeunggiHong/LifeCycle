fea8849f07a4b1c8b24e4d70d8bc08a15c9ccc24
SeunggiHong <tmdrl1026@naver.com>
2017-09-27 16:52:33 +0900

    first test

---
+++ app/src/main/java/com/example/hsg/lifecycle/LifeCycleActivity.java
@@ -0,0 +1,54 @@
+package com.example.hsg.lifecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class LifeCycleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_life_cycle);
+
+        Log.i("MY","--onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MY","--onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MY","--onStop");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MY","--onResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MY","--onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("MY","--onRestart");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MY","--onDestroy");
+    }
+}
